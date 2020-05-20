abd02cfa09c28eecbf7c65f61342e9836bd09b27
ThemeGrill <milanthapa299@gmail.com>
2020-05-20 21:44:46 +0545

    Added required CSS and enqueues CSS and JS files

--- inc/admin/class-fitclub-admin.php
+++ inc/admin/class-fitclub-admin.php
@@ -19,6 +19,24 @@ if ( ! class_exists( 'FitClub_Admin' ) ) :
 		 */
 		public function __construct() {
 			add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		}
+
+		/**
+		 * Localize array for import button AJAX request.
+		 */
+		public function enqueue_scripts() {
+			wp_enqueue_style( 'fitclub-admin-style', get_template_directory_uri() . '/inc/admin/css/admin.css', array(), FOODHUNT_THEME_VERSION );
+
+			wp_enqueue_script( 'fitclub-plugin-install-helper', get_template_directory_uri() . '/inc/admin/js/plugin-handle.js', array( 'jquery' ), FOODHUNT_THEME_VERSION, true );
+
+			$welcome_data = array(
+				'uri'      => esc_url( admin_url( '/themes.php?page=demo-importer&browse=all&fitclub-hide-notice=welcome' ) ),
+				'btn_text' => esc_html__( 'Processing...', 'fitclub' ),
+				'nonce'    => wp_create_nonce( 'fitclub_demo_import_nonce' ),
+			);
+
+			wp_localize_script( 'fitclub-plugin-install-helper', 'fitclubRedirectDemoPage', $welcome_data );
 		}
 
 		/**
@@ -36,7 +54,7 @@ if ( ! class_exists( 'FitClub_Admin' ) ) :
 					'welcome_screen',
 				)
 			);
-			add_action( 'admin_print_styles-' . $page, array( $this, 'enqueue_styles' ) );
+			add_action( 'admin_print_styles-' . $page, array( $this, 'enqueue_scripts' ) );
 		}
 
 		/**
